5887e38ca785723a2951dc6b22086c32bd8df250
Sumerian Code <102862728+Ztech8@users.noreply.github.com>
2023-12-13 20:44:46 +0300

    Update RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -1,5 +1,23 @@
-a = '''
-       
+import random
+
+def generate_password(length, all_chars):
+    return "".join(random.sample(all_chars, length))
+
+def save_passwords_to_file(passwords, filename):
+    if not filename.lower().endswith('.txt'):
+        filename += '.txt'
+    
+    try:
+        with open(filename, 'w') as file:
+            for password in passwords:
+                file.write(password + '\n')
+        print(f"Passwords saved to {filename}")
+    except Exception as e:
+        print(f"Error saving passwords: {e}")
+
+def print_banner():
+    banner = """
+    
  /$$$$$$$  /$$$$$$$   /$$$$$$ 
 | $$__  $$| $$__  $$ /$$__  $$
 | $$  \ $$| $$  \ $$| $$  \__/
@@ -9,23 +27,15 @@ a = '''
 | $$  | $$| $$      |  $$$$$$/
 |__/  |__/|__/       \______/ 
                               
-                                 developed by Sumerian Code
-
-    '''
-
-print(a)
-print("_" * 80)
-
-
-
-import random 
-
-print("")
+                              
+                              
+    """
+    print(banner)
 
 uppercase_letters = "ABCDEFGHIOPQLNVZXWJKLRTYU"
 lowercase_letters = uppercase_letters.lower()
 digits = "0123456789"
-symbols = "!@#$%^&*_?/\-+"
+symbols = "!@#$%^&*_?-/+"
 
 upper, lower, nums, syms = True, True, True, True
 
@@ -40,13 +50,23 @@ if nums:
 if syms:
     all_chars += symbols
 
-print("")
-print("")
+print_banner()
 
-length = int(input("Enter the length of the password >> "))
-print("")
-amount = int(input("Enter how many passwords to create >> "))
+try:
+    length = int(input("Enter the length of the password: "))
+    amount = int(input("Enter how many passwords to create: "))
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+    exit()
+
+passwords = []
 
 for x in range(amount):
-    password = "".join(random.sample(all_chars, length))
+    password = generate_password(length, all_chars)
+    passwords.append(password)
     print(password)
+
+save_passwords_option = input("\nDo you want to save these passwords to a file? (yes/no): ").lower()
+if save_passwords_option in ['y', 'yes']:
+    filename = input("Enter the filename to save passwords (without extension): ")
+    save_passwords_to_file(passwords, filename)
