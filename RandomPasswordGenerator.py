ef219a46eb83e854ac8f37a9b8af4973d5f7c078
Sumerian Code <102862728+Ztech8@users.noreply.github.com>
2023-12-13 20:02:18 +0300

    Update RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -1,5 +1,5 @@
-def banner():
-    a = '''
+
+a = '''
        
  /$$$$$$$  /$$$$$$$   /$$$$$$ 
 | $$__  $$| $$__  $$ /$$__  $$
@@ -14,42 +14,40 @@ def banner():
 
     '''
 
-    print(a)
-    print("_" * 80)
-banner()
+print(a)
+print("_" * 80)
+
+
 
 import random 
+
 print("")
 
-
-
-uppercase_letters = ["ABCDEFGHIOPQLNVZXWJKLRTYU"]
+uppercase_letters = "ABCDEFGHIOPQLNVZXWJKLRTYU"
 lowercase_letters = uppercase_letters.lower()
-digits = (1,2,3,4,5,6,7,8,9)
-symbols = ("!@#$%^&*_?/")
+digits = "123456789"
+symbols = "!@#$%^&*_?/"
 
-upper, lower, nums, syms =True ,True, True, True
+upper, lower, nums, syms = True, True, True, True
 
-all = ""
+all_chars = ""
 
-if upper == str:
-    all += uppercase_letters 
-if lower == str: 
-    all +=  lowercase_letters 
-if nums: 
-    all += digits 
-if syms == str: 
-    all += symbols 
+if upper:
+    all_chars += uppercase_letters
+if lower:
+    all_chars += lowercase_letters
+if nums:
+    all_chars += digits
+if syms:
+    all_chars += symbols
 
 print("")
 print("")
 
-
-length = int(input(("Enter the length of the password >> ")))
+length = int(input("Enter the length of the password >> "))
 print("")
 amount = int(input("Enter how many passwords to create >> "))
 
 for x in range(amount):
-    password = "".join(random.sample(all, length)) 
+    password = "".join(random.sample(all_chars, length))
     print(password)
-
